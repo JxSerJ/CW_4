0d32863144b8364546325ae9f65e3b61d81f7cd6
S.P <sergey.a.protsenko@mail.ru>
2022-06-09 07:42:53 +0700

    director and movie initital services created

--- project/services/__init__.py
+++ project/services/__init__.py
@@ -1,3 +1,9 @@
 from .genres_service import GenresService
+from .directors_service import DirectorService
+from .movies_service import MovieService
 
-__all__ = ["GenresService"]
+__all__ = [
+    "GenresService",
+    "DirectorService",
+    "MovieService"
+]
